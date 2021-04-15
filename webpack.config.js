0abc1e2ad8e6131f479861b719464f4305f9c047
Kanasimy <admin@active-way.ru>
2021-04-15 14:49:20 +0700

    настройка сборки

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+//Встроенный модуль, предоставляет набор функций для работы с путями в файловой системе
 const path = require("path");
+//реализует ввод/вывод файлов
 const fs = require("fs");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
@@ -6,13 +8,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
 function generateHtmlPlugins(templateDir) {
+  //path.resolve принимает составные части пути и возвращает абсолютный путь полученного в результате обработки переданных сегментов пути.
+  //fs.readdirSync чтение содержимого папки — то есть — сведений о том, какие файлы и поддиректории в ней имеются, и возврат их относительных путей
+  //т.е. templateFiles содержит пути к файлам и директориям
   const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
   return templateFiles.map((item) => {
     const parts = item.split(".");
+    //тут у нас имя файла
     const name = parts[0];
+    //расширение
     const extension = parts[1];
+    //
     return new HtmlWebpackPlugin({
       filename: `${name}.html`,
       template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
@@ -81,6 +90,19 @@ const config = {
           pretty: true
         }
       },
+      {
+        //не поняла почему не использует путь из общих настроек path: path.resolve(__dirname, "dist") для publicPath   ,
+        //ну и собственно не вижу результата
+        test: /\.svg$/,
+        include: path.resolve(__dirname, "src/svg"),
+        loader: 'svg-sprite-loader',
+        options: {
+          extract: true,
+          publicPath: path.resolve(__dirname, 'dist/img'),
+          spriteFilename: 'sprite.svg'
+        }
+
+      }
     ],
   },
   plugins: [
@@ -97,16 +119,13 @@ const config = {
           from: "./src/favicon",
           to: "./favicon",
         },
-        {
-          from: "./src/img",
-          to: "./img",
-        },
-        {
-          from: "./src/uploads",
-          to: "./uploads",
-        },
+         {
+           from: "./src/img",
+           to: "./img",
+         },
       ],
     }),
+    new SpriteLoaderPlugin({plainSprite: true}),
   ].concat(htmlPlugins),
 };
 
